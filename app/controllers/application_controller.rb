b36bf8ade45f9e8e746aae1e109505f4a0a61130
longtt4902 <longtt@zigexn.vn>
2024-12-25 14:27:04 +0700

    format code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :type])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:type])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :type ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :type ])
   end
 
   def after_sign_in_path_for(resource)
