913a299ea84f2b254d6e26d9da81c2e3b42e0d8a
longtt4902 <longtt@zigexn.vn>
2025-01-21 18:37:10 +0700

    finish logic review test after submitting

--- app/controllers/user/dashboards_controller.rb
+++ app/controllers/user/dashboards_controller.rb
@@ -17,7 +17,6 @@ class User::DashboardsController < User::BaseController
 
     submitted_answers = params[:test]
     @correct_answers = 0;
-    binding.pry
 
     submitted_answers.each do |question_id, answer_id|
       question = Question.find(question_id.to_i)
@@ -28,19 +27,30 @@ class User::DashboardsController < User::BaseController
       end
       answer = question.answers.find_by(id: answer_id["answer_id"].to_i)
 
-      # Lưu câu trả lời vào user_answers
-      user_exam.user_answers.create!(
-        question_id: question.id,
-        answer_id: answer&.id,
-        times: user_exam.user_answers.where(question_id: question.id).count + 1
-      )
+      if user_exam.user_answers.exists?(question_id: question.id)
+        user_answer = user_exam.user_answers.find_by(question_id: question.id)
+        user_answer.update!(
+          answer_id: answer&.id,
+          times: user_answer.times + 1
+        )
+      else
+        user_exam.user_answers.create!(
+          question_id: question.id,
+          answer_id: answer&.id,
+          times: 1
+        )
+      end
     end
 
     @total_questions = submitted_answers.keys.size
-    @score = ((@correct_answers.to_f / @total_questions) * 10).round(2)
-    user_exam.update(score: @score, submit_time: Time.current)
+    score = ((@correct_answers.to_f / @total_questions) * 10).round(2)
+    user_exam.update(score: score, submit_time: Time.current)
+
+    @user_exam = user_exam
+    @user_answers = user_exam.user_answers
+
     render json: {
-      html: render_to_string( partial: "result", locals: { total_questions: @total_questions, score: @score }, formats: [ :html ], layout: false )
+      html: render_to_string( partial: "result", locals: { total_questions: @total_questions, score: @user_exam.score }, formats: [ :html ], layout: false )
     }
   end
 end
